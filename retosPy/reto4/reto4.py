6840e3381df2fdcc7b1f630e0b60ad4468c24170
Pablo <velazquezpabloes03@gmail.com>
2022-12-07 09:16:14 +0100

    Reto4 Python

--- retosPy/reto4/reto4.py
+++ retosPy/reto4/reto4.py
@@ -1,30 +1,34 @@
+#Funcion:
 def fitsInOneBox(boxes):
     #Input:Array con las dimensiones de la caja ({ l: 1, w: 1, h: 1 })
-    #Output: True o false si es posible empaquetar una caja en otra
-    ls = []
+    #Output: True o false si es posible empaquetar una caja en otra.
+    listTam = []
     #Lista con la suma de las dimensiones de cada caja
     for e in boxes:
-        ls.append(sum(e.values()))
-    indexMaxBox = ls.index(max(ls)) #Saca el mayor indice
-    maxBox = boxes[indexMaxBox]     #Saca el mayor diccionario
-    boxes.pop(indexMaxBox)
-    i = 0
-    while(i<len(boxes)):
-        if((maxBox.get("l") > boxes[i].get("l")) & (maxBox.get("w") > boxes[i].get("w")) & (maxBox.get("h") > boxes[i].get("h"))):
+        listTam.append(sum(e.values()))
+    #Lista tam ordenada de mayor a menor:
+    listTamOrd = sorted(listTam, reverse=True)
+    #Lista con las cajas ordenadas por dimensiones:
+    boxesOrd = [boxes[listTam.index(listTamOrd[i])] for i in range(len(listTam))]
+    #Bucle para comprobar si una caja cabe en otra:
+    for i  in range(1, len(boxesOrd)):
+        if((boxesOrd[i].get("l") < boxesOrd[i-1].get("l")) & (boxesOrd[i].get("w") < boxesOrd[i-1].get("w")) & (boxesOrd[i].get("h") < boxesOrd[i-1].get("h"))):
             res = True
-            maxBox = boxes[i]
-            i+=1
-        else: 
+        else:
             res = False
             break
     return res
 
+#Funcion Test:
+def fitsInOneBox_Test(boxes):
+    boxes1= [ { "l": 3, "w": 3, "h": 3 }, { "l": 2, "w": 2, "h": 2 }, { "l" : 1, "w": 1, "h": 1 }]
+    boxes2 = [ { "l" : 1, "w": 1, "h": 1 },  { "l": 3, "w": 1, "h": 3 }, { "l": 2, "w": 2, "h": 2 }]
+    print(fitsInOneBox(boxes1))#True
+    print(fitsInOneBox(boxes2))#False
         
 
 
 
 if __name__ == "__main__":
     boxes1= [ { "l": 3, "w": 3, "h": 3 }, { "l": 2, "w": 2, "h": 2 }, { "l" : 1, "w": 1, "h": 1 }]
-    boxes2 = [ { "l" : 1, "w": 1, "h": 1 },  { "l": 3, "w": 1, "h": 3 }, { "l": 2, "w": 2, "h": 2 }]
-    print(fitsInOneBox(boxes1))
-    #print(boxes[0].values())
+    fitsInOneBox_Test()
